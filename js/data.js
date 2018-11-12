28422daaa8e010a1d34c859d05fd37541d8ea04e
hassan <baziegar@gmail.com>
2018-11-12 19:29:41 +0500

    adding data.js,knockout.js,main.js files

---
+++ js/data.js
@@ -0,0 +1,46 @@
+/* Model Data */
+
+var locations = [
+    {
+        title: 'San Luis Obispo County',
+        location: {
+            lat: 35.376529,
+            lng: -120.602997
+        }
+    },
+    {
+        title: 'Morro Bay',
+        location: {
+            lat: 35.357950,
+            lng: -120.831653
+        }
+    },
+    {
+        title: 'Los Osos',
+        location: {
+            lat: 35.306141,
+            lng: -120.811374
+        }
+    },
+    {
+        title: 'Tomasini Rd',
+        location: {
+            lat: 35.349980,
+            lng: -120.769172
+        }
+    },
+    {
+        title: 'Baywood-Los Osos',
+        location: {
+            lat: 35.311842,
+            lng: -120.822310
+        }
+    },
+    {
+        title: 'Cayucos',
+        location: {
+            lat: 35.422218,
+            lng: -120.875130
+        }
+    }
+];
